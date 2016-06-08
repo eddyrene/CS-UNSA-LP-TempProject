2714180afdb55a41125105b64f299c56812697bc
JuanjoLopez <juanjolopez28@gmail.com>
2016-06-07 20:41:01 -0600

    Consulta
    La consulta de búsqueda esta terminada

--- myflaskapp/mongo.py
+++ myflaskapp/mongo.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 import re
 import io
 from pymongo import MongoClient
@@ -17,19 +18,20 @@ class mongo:
         except ValueError:
             print ('error en la conexion')
 
-    def insert_cuarto(self,ID,coord,servicios,nombre,precio,genero):
+    def insert_cuarto(self,ID,Distrito,coord,servicios,nombre,precio,genero):
         vivienda ={
                 "ID":ID,
-                "Coord":[coord[0],coord[1]],
+                "Distrito":Distrito,
+                "Coord":{'coord1':coord[0],'coord2':coord[1]},#dos coordenadas
                 "Nombre":nombre,
-                "Servicios":[servicios[0] , servicios[1] ,servicios[2] ],
+                "Servicios":{'baño':servicios[0] ,'tv':servicios[1] ,'ducha':servicios[2],'wifi':servicios[3]},#baño,tv,ducha,mascota con 0 y 1
                 "Precio":precio,
-                "Genero":genero
+                "Genero":genero# 1 es solo hombres 2 es solo chicas y tres es los dos
                    }
         try:
             self.db.insert_one(vivienda).inserted_id
         except ValueError:
-            print ('No se pudo insertar')
+            print ('No se pudo insertear')
             
     def mostrar(self):
         results=self.db.find()
@@ -41,18 +43,15 @@ class mongo:
     def eliminar (self,ID):#probar
         self.db.delete_one({'ID':ID})
         
-    '''def mas_baratos(self,precio_min,precio_max):#podemos modificarlo para una solo universidad
-        result=self.db.find({'Precio':{'$gte':precio_min}},'$and:'{'Precio':{'$lte':precio_min}}})
-        for record in result:
-            print(record['Nombre'])'''
-    
-    
     def desconectar(self):
         self.connection.close()
-
-
-
-#if __name__ == "__main__":
+        
+    def mas_baratos(self,distrito,genero,servicios,precio_min,precio_max):#podemos modificarlo para una solo universidad
+        result=self.db.find({'Distrito':distrito,'Genero':genero,'Servicios.baño':servicios[0],'Servicios.tv':servicios[1],'Servicios.ducha':servicios[2],'Servicios.wifi':servicios[3],'$and': [{'Precio':{'$gte':precio_min}},{'Precio':{'$lte':precio_max}}]})
+        for record in result:
+           print record['ID']
+    #baño,tv,ducha,wifi
+if __name__ == "__main__":
     #connection = MongoClient("mongodb://juanjo:1234@ds015713.mlab.com:15713/prueba")
     #db = connection.prueba.docs101
     '''filename = "C:\Users\Juanjo\Desktop\casa.png"
@@ -67,10 +66,15 @@ class mongo:
     output= open(outfilename,"w")     
     output.write(outputdata)
     output.close()#imagen'''
-    #mongo1=mongo()
-    #mongo1.insert_cuarto('1',['123','12'],['1','0','1','1','0'],'Seoa','70','3')
-    #mongo1.insert_cuarto('1',['123','12'],['1','0','1','1','0'],'Seoa','50','3')
-    #print mongo1.mas_baratos('70',100)
+    mongo1=mongo()
+    mongo1.mas_baratos('Avelino','3',['1','0','1','0'],'100','130')#distrito,genero(1 o 2 o 3),servicios(baño,tv,ducha,wifi),precio min ,precio max)
+    mongo1.desconectar()
+    '''mongo1.insert_cuarto('1','Cayma',['123','12'],['1','0','1','1'],'Seoa','300','3')
+    mongo1.insert_cuarto('2','Cayma',['123','12'],['1','1','1','1'],'Seoa','70','1')
+    mongo1.insert_cuarto('3','Avelino',['123','12'],['1','0','1','0'],'Seoa','120','3')
+    mongo1.insert_cuarto('4','Avelino',['123','12'],['1','1','1','1'],'Seoa','100','3')
+    mongo1.insert_cuarto('5','Avelino',['123','12'],['1','0','1','0'],'Seoa','180','3')'''
+    #mongo1.mas_baratos('70','100')
     #mongo1.mostrar()
     #mongo1.desconectar()
     #mongo1.mostrar
