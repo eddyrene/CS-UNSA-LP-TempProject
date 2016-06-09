68134481cd6ec575ff6311b0e6d4a65f1464f491
Alexandra1123 <al.h8500@gmail.con>
2016-06-09 14:54:57 -0500

    Filtros

--- Alquilercom2/app/src/main/java/com/example/alquiler/alquilercom/Filtros.java
+++ Alquilercom2/app/src/main/java/com/example/alquiler/alquilercom/Filtros.java
@@ -2,6 +2,8 @@ package com.example.alquiler.alquilercom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Filtros extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class Filtros extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtros);
+        Button mAplicar = (Button) findViewById(R.id.button);
+        
+        mAplicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AplicarCambios();
+            }
+        });
+    }
+
+    private void AplicarCambios(){
+        return;
     }
 }
