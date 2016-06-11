8b8548b7da611afbde11bf59c64ce84db16bb4bb
JuanjoLopez <juanjolopez28@gmail.com>
2016-06-11 11:22:34 -0600

    Funciones para la imágenes
    Estas funciones permiten guardar y sacar de la base de datos las
    imagenes ,mmm las guarda en binario.

--- myflaskapp/mongo.py
+++ myflaskapp/mongo.py
@@ -5,15 +5,19 @@ from pymongo import MongoClient
 import gridfs
 
 #bd_url="mongodb://alexandra:alexandra@ds015953.mlab.com:15953/code101"
-bd_url="mongodb://juanjo:1234@ds015713.mlab.com:15713/prueba"
+bd_url="mongodb://juanjo:1234@ds015713.mlab.com:15713/prueba"#es mi url trabajare con ella
 class mongo:
     db=0
     connection=0
     viviendas=[]
+    db_img=0
+    fs=0
     def __init__(self):
         try:
             self.connection = MongoClient(bd_url)
             self.db = self.connection.prueba.Cuartos
+            self.db_img=self.connection.prueba
+            self.fs = gridfs.GridFS(self.db_img)
             print ('conexion sin problemas')
         except ValueError:
             print ('error en la conexion')
@@ -21,7 +25,7 @@ class mongo:
         vivienda ={
                 "ID":ID,
                 "Distrito":Distrito,
-                "Coord":{'coord1':coord[0],'coord2':coord[1]},#dos coordenadas
+                "Coord":{'coord1':coord[0],'coord2':coord[1]},#dos coordenadas posicion en el mapa
                 "Nombre":nombre,
                 "Servicios":{'baño':servicios[0] ,'tv':servicios[1] ,'ducha':servicios[2],'wifi':servicios[3]},#baño,tv,ducha,mascota con 0 y 1
                 "Precio":precio,
@@ -49,6 +53,20 @@ class mongo:
         result=self.db.find({'Distrito':distrito,'Genero':genero,'Servicios.baño':servicios[0],'Servicios.tv':servicios[1],'Servicios.ducha':servicios[2],'Servicios.wifi':servicios[3],'$and': [{'Precio':{'$gte':precio_min}},{'Precio':{'$lte':precio_max}}]})
         for record in result:
            print record['ID']
+    def insertar_imagen(self,path,nombre):
+        
+        filename = path #este un ejemplo "C:\Users\Juanjo\Desktop\casa.png"
+        datafile = open(filename,"rb");
+        thedata = datafile.read()
+        self.fs.put(thedata, filename=nombre)
+        datafile.close()
+    def sacar_imagen(self,nombre,path,nuevo_nombre):#el nombre con el q se guardo en la BD
+        path=path+"\ "+nuevo_nombre
+        outputdata =self.fs.get_last_version(nombre).read()
+        outfilename = path#donde se guardara la imagen ejemplo "C:\Users\Juanjo\Desktop\casa.png"
+        output= open(outfilename,"wb")     
+        output.write(outputdata)
+        output.close()
     #baño,tv,ducha,wifi
 if __name__ == "__main__":
     #connection = MongoClient("mongodb://juanjo:1234@ds015713.mlab.com:15713/prueba")
@@ -66,8 +84,10 @@ if __name__ == "__main__":
     output.write(outputdata)
     output.close()#imagen'''
     mongo1=mongo()
-    mongo1.mas_baratos('Avelino','3',['1','0','1','0'],'100','130')#distrito,genero(1 o 2 o 3),servicios(baño,tv,ducha,wifi),precio min ,precio max)
-    mongo1.desconectar()
+    #mongo1.mas_baratos('Avelino','3',['1','0','1','0'],'100','130')#distrito,genero(1 o 2 o 3),servicios(baño,tv,ducha,wifi),precio min ,precio max)
+    #mongo1.desconectar()
+    #mongo1.insertar_imagen("C:\Users\Juanjo\Desktop\casa.png",'casa')
+    mongo1.sacar_imagen('casa','C:\Users\Juanjo\Desktop','casa_5.png')
     '''mongo1.insert_cuarto('1','Cayma',['123','12'],['1','0','1','1'],'Seoa','300','3')
     mongo1.insert_cuarto('2','Cayma',['123','12'],['1','1','1','1'],'Seoa','70','1')
     mongo1.insert_cuarto('3','Avelino',['123','12'],['1','0','1','0'],'Seoa','120','3')
