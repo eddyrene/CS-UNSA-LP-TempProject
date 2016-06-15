dd4db9795e6cf5ed838042b49abd514f22ab50f2
eddyrene <eddy.rene.c@gmail.com>
2016-06-14 20:52:51 -0500

    avanze id
    Se adecuo el scroll view de Filtros
    Se termino precios de Filtros
    Se cambio icno de aplicacion
    se creo activiy register (falta unir con app)
    Es un avanze para que Ale puedas ir implementando lo de los precios

--- Alquilercom2/app/src/main/java/com/example/alquiler/alquilercom/Filtros.java
+++ Alquilercom2/app/src/main/java/com/example/alquiler/alquilercom/Filtros.java
@@ -2,19 +2,57 @@ package com.example.alquiler.alquilercom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Filtros extends AppCompatActivity {
 
+    private SeekBar seekBar;
+    private TextView textViewSeekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtros);
+
+        //Parte del scroll!!!!
+        seekBar = (SeekBar) findViewById(R.id.seekBar_precio);
+        textViewSeekBar= (TextView) findViewById(R.id.textView_precio);
+        textViewSeekBar.setText("80");
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                //la Seekbar siempre empieza en cero, si queremos que el valor mínimo sea otro podemos modificarlo
+                textViewSeekBar.setText(progress + 80 + "");
+            }
+
+            /**
+             * El usuario inicia la interacción con la Seekbar.
+             */
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0)
+            {
+            }
+            /**
+             * El usuario finaliza la interacción con la Seekbar.
+             */
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0)
+            {
+
+            }
+        });
+
+        //Parte del boton aceptar!!!!!
         Button mAplicar = (Button) findViewById(R.id.button);
-        
         mAplicar.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View v) {
                 AplicarCambios();
             }
